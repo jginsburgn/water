5a2fcdf4ac5c3df162983966fa908030d3dac96f
Jonathan Ginsburg <jginsburgn@gmail.com>
2019-05-20 07:42:13 -0500

    Optimize Neighborhood Search By Voxel Dictionary

--- src/SPH/Fluid.ts
+++ src/SPH/Fluid.ts
@@ -1,40 +1,73 @@
-import { Group } from "three";
+import { Group, Vector3 } from "three";
 import { Particle, Particles } from "./Particle";
+import { subscribeToAnimationLoop } from "../general";
+
+export type Voxels = { [voxelSignature: string]: Array<Particle> };
 
 export class Fluid extends Group {
-  particles: Particles = [];
+  static readonly VOXEL_EDGE_LENGTH: number = 1;
+
+  voxels: Voxels = {};
 
   constructor() {
     super();
-    for (let i = 0; i < 10; i++) {
-      for (let j = 0; j < 5; j++) {
-        for (let k = 0; k < 10; k++) {
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 25; j++) {
+        for (let k = 0; k < 5; k++) {
           const p = new Particle(this);
-          const x = i * Particle.PARTICLE_RADIUS;
-          const y = j * Particle.PARTICLE_RADIUS;
-          const z = k * Particle.PARTICLE_RADIUS;
+          const x = i;
+          const y = j;
+          const z = k;
           p.position.set(x, y, z);
-          this.particles.push(p);
           this.add(p);
         }
       }
     }
+    subscribeToAnimationLoop((at, td) => {
+      this.step(at, td);
+    });
   }
 
-  getParticles(): Particles {
-    return this.particles;
+  buildVoxels() {
+    this.voxels = {};
+    for (let p of this.children) {
+      const particle = p as Particle;
+      particle.shadowPosition.copy(particle.position);
+      particle.clearCaches();
+      const voxelSignature = particle.getVoxelSignature();
+      if (!this.voxels[voxelSignature]) {
+        this.voxels[voxelSignature] = [];
+      }
+      this.voxels[voxelSignature].push(particle);
+    }
+  }
+
+  step(absoluteAccumulatedTime: number, timeDifference: number) {
+    this.buildVoxels();
+    for (let p of this.children) {
+      const particle = p as Particle;
+      particle.step(absoluteAccumulatedTime, timeDifference);
+    }
   }
 
   getNeighborhoodFor(particle: Particle): Particles {
-    // TODO: Do efficient neighborhood search by voxels
-    return this.getParticles().reduce(
-      (prev, current) => {
-        if (particle != current && particle.withinSmoothingRadius(current.position)) {
-          prev.push(current);
+    const neighborhoodVoxelSignatures = particle.getNeighborhoodVoxelSignatures();
+    const neighborhood = [];
+    for (let voxelSignature of neighborhoodVoxelSignatures) {
+      const voxel = this.voxels[voxelSignature];
+      if (!voxel) {
+        continue;
+      }
+      for (let p of voxel) {
+        const currentParticle = p as Particle;
+        if (currentParticle == particle) {
+          continue;
         }
-        return prev;
-      },
-      []
-    );
+        if (currentParticle.withinSmoothingRadius(currentParticle.shadowPosition)) {
+          neighborhood.push(currentParticle);
+        }
+      }
+    }
+    return neighborhood;
   }
 }
--- src/SPH/Particle.ts
+++ src/SPH/Particle.ts
@@ -1,16 +1,16 @@
 import { Object3D, Vector3, MeshBasicMaterial, Mesh, SphereGeometry } from "three";
 import { Fluid } from "./Fluid";
-import { subscribeToAnimationLoop } from "../general";
 
 export type Particles = Array<Particle>;
 
 export class Particle extends Object3D {
 
   // Physical properties
-  static readonly VISCOSITY_COEFFICIENT: number = 5; // µ
+  static readonly VISCOSITY_COEFFICIENT: number = 0.5; // µ
   static readonly DENSITY_COEFFICIENT /*aka stiffness*/: number = 10; // k
-  static readonly BASE_DENSITY: number = 998; // ρ_0
-  static readonly SMOOTHING_RADIUS: number = 16; // h
+  static readonly BASE_DENSITY: number = 0.2; // ρ_0
+  static readonly SMOOTHING_RADIUS: number = 1.5; // h
+  static readonly MAXIMUM_SPEED: number = 9;
 
   // Mathematical shortcuts
   static readonly SPH_DENSITY_COEFFICIENT: number = 315 / (64 * Math.PI * Particle.SMOOTHING_RADIUS ** 9);
@@ -18,48 +18,92 @@ export class Particle extends Object3D {
   static readonly SPH_VISCOSITY_COEFFICIENT: number = - Particle.SPH_PRESSURE_GRADIENT_COEFFICIENT;
 
   // Drawing matters
-  static readonly PARTICLE_RADIUS: number = Particle.SMOOTHING_RADIUS / 2;
+  static readonly PARTICLE_RADIUS: number = 1;
   static readonly GEOMETRY = new SphereGeometry(Particle.PARTICLE_RADIUS * 0.3);
-  static readonly MATERIAL = new MeshBasicMaterial({color: 0x2FA1D6, opacity: 0.5, transparent: true});
+  static readonly MATERIAL = new MeshBasicMaterial({ color: 0x2FA1D6, opacity: 0.8, transparent: true });
   static readonly MESH = new Mesh(Particle.GEOMETRY, Particle.MATERIAL);
 
   fluid: Fluid = undefined;
-  mass: number = 500 * .13;
+  mass: number = 0.5;
   velocity: Vector3 = new Vector3();
+  shadowPosition: Vector3 = new Vector3();
+
+  private cachedNeighborhood: Particles = undefined;
+  private cachedDensity: number = undefined;
 
   constructor(fluid: Fluid) {
     super();
     this.fluid = fluid;
     this.add(Particle.MESH.clone());
-    subscribeToAnimationLoop((at, td) => { this.step(at, td) });
   }
 
   withinSmoothingRadius(position: Vector3): boolean {
     return this.distanceTo(position) < Particle.SMOOTHING_RADIUS;
   }
 
+  clearCaches() {
+    // Stale caches
+    this.cachedNeighborhood = undefined;
+    this.cachedDensity = undefined;
+  }
+
+  getVoxelIndices(): Vector3 {
+    const vel = Fluid.VOXEL_EDGE_LENGTH;
+    const vi = this.shadowPosition.clone();
+    const x = Math.floor(vi.x / vel);
+    const y = Math.floor(vi.y / vel);
+    const z = Math.floor(vi.z / vel);
+    vi.set(x, y, z);
+    return vi;
+  }
+
+  getVoxelSignature(): string {
+    return this.getVoxelIndices().toArray().toString();
+  }
+
+  getNeighborhoodVoxelSignatures(): Array<string> {
+    const voxelSignatures = [];
+    const voxelIndices = this.getVoxelIndices();
+    for (let i = -1; i < 2; i++) {
+      for (let j = -1; j < 2; j++) {
+        for (let k = -1; k < 2; k++) {
+          const x = voxelIndices.x + i;
+          const y = voxelIndices.y + j;
+          const z = voxelIndices.z + k;
+          const voxelSignature = new Vector3(x, y, z).toArray().toString();
+          voxelSignatures.push(voxelSignature);
+        }
+      }
+    }
+    return voxelSignatures;
+  }
+
   getNeighborhood(): Particles {
-    return this.fluid.getNeighborhoodFor(this);
+    if (!this.cachedNeighborhood) {
+      this.cachedNeighborhood = this.fluid.getNeighborhoodFor(this);
+    }
+    return this.cachedNeighborhood as Particles;
   }
 
   distanceTo(position: Vector3): number {
-    const distance = this.position.clone().sub(position).length();
+    const distance = this.shadowPosition.clone().sub(position).length();
     return distance;
   }
 
   getDensity(): number {
-    const neighborhood = this.getNeighborhood();
-    neighborhood.push(this);
-    const density = neighborhood.reduce(
-      (accumulated, current) => {
-        const distance = this.distanceTo(current.position);
-        const mass = current.mass;
-        const contribution = Particle.SPH_DENSITY_COEFFICIENT * mass * (Particle.SMOOTHING_RADIUS ** 2 - distance ** 2) ** 3;
-        return accumulated + contribution;
-      },
-      0
-    );
-    return density;
+    if (!this.cachedDensity) {
+      const neighborhood = this.getNeighborhood().concat(this);
+      this.cachedDensity = neighborhood.reduce(
+        (accumulated, current) => {
+          const distance = this.distanceTo(current.shadowPosition);
+          const mass = current.mass;
+          const contribution = Particle.SPH_DENSITY_COEFFICIENT * mass * (Particle.SMOOTHING_RADIUS ** 2 - distance ** 2) ** 3;
+          return accumulated + contribution;
+        },
+        0
+      );
+    }
+    return this.cachedDensity;
   }
 
   getPressure(): number {
@@ -68,18 +112,17 @@ export class Particle extends Object3D {
   }
 
   getExternalForceTerm(): Vector3 {
-    // Gravity for now
-    return new Vector3(0, -120000 * 9.82 * this.getDensity(), 0);
+    return new Vector3(0, -9.82, 0);
   }
 
   getPressureGradientTerm(): Vector3 {
     return this.getNeighborhood().reduce(
       (accumulated, current) => {
-        const distance = this.distanceTo(current.position);
+        const distance = this.distanceTo(current.shadowPosition);
         const mass = current.mass;
         const pressureFactor = this.getPressure() / this.getDensity() ** 2 + current.getPressure() / current.getDensity() ** 2;
         const smoothingRadiusDistanceFactor = (Particle.SMOOTHING_RADIUS - distance) ** 2;
-        const normalizedPositionDifference = current.position.clone().sub(this.position).normalize();
+        const normalizedPositionDifference = current.shadowPosition.clone().sub(this.shadowPosition).normalize();
         const contributionScale = mass * pressureFactor * Particle.SPH_PRESSURE_GRADIENT_COEFFICIENT * smoothingRadiusDistanceFactor;
         const contribution = normalizedPositionDifference.multiplyScalar(contributionScale);
         return accumulated.add(contribution);
@@ -91,7 +134,7 @@ export class Particle extends Object3D {
   getViscousTerm(): Vector3 {
     const viscousTerm = this.getNeighborhood().reduce(
       (accumulated, current) => {
-        const distance = this.distanceTo(current.position);
+        const distance = this.distanceTo(current.shadowPosition);
         const mass = current.mass;
         const smoothingRadiusDistanceFactor = Particle.SMOOTHING_RADIUS - distance;
         const currentDensity = current.getDensity();
@@ -118,14 +161,37 @@ export class Particle extends Object3D {
   }
 
   step(absoluteAccumulatedTime: number, timeDifference: number) {
-    // TODO: Implement boundary conditions
-    const newVelocity = this.velocity.clone().add(this.getAcceleration().multiplyScalar(0.006));
-    const newPosition = this.position.clone().add(this.velocity.clone().multiplyScalar(0.006));
-    // Temporary floor boundary condition
-    if (newPosition.y < -10) {
-      newVelocity.multiplyScalar(-0.8);
+    // Velocity
+    const newVelocity = this.velocity.clone().add(this.getAcceleration().multiplyScalar(0.005));
+
+    // Speed limit
+    const speed = newVelocity.length();
+    if (speed > Particle.MAXIMUM_SPEED) {
+      const multiplier = Particle.MAXIMUM_SPEED / speed;
+      newVelocity.multiplyScalar(multiplier);
     }
+
+    // Position
+    const newPosition = this.position.clone().add(this.velocity.clone().multiplyScalar(0.005));
+
+    // Boundary conditions
+    const xCondition = newPosition.x < -50 || newPosition.x > 55;
+    if (xCondition) {
+      newVelocity.setX(-1 * newVelocity.x);
+    }
+    const yCondition = newPosition.y < -10;
+    if (yCondition) {
+      newVelocity.setY(-0.6 * newVelocity.y);
+    }
+    const zCondition = newPosition.z < 0 || newPosition.z > 5;
+    if (zCondition) {
+      newVelocity.setZ(-1 * newVelocity.z);
+    }
+
+    // Numerical integration
     this.velocity.copy(newVelocity);
-    this.position.copy(newPosition);
+    if (!(xCondition || yCondition || zCondition)) {
+      this.position.copy(newPosition);
+    }
   }
 }
